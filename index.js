d035ebcb2abda31b6ea1a372b639bfbc703e4ddc
Yannick Lohse <contact@yannick-lohse.fr>
2017-04-06 16:48:55 +0200

    feat: added drop command

--- index.js
+++ index.js
@@ -78,6 +78,25 @@ const getClientWithToken = (url, docTypes) => {
   })
 }
 
+//convenience wrapper around the 2 client getters
+const getClient = (generateNewToken, cozyUrl, docTypes) => {
+  //now get a client
+  const getClientFn = generateNewToken ? getClientWithoutToken : getClientWithToken;
+
+  return getClientFn(cozyUrl, docTypes)
+  .then(client => {
+    return client;
+  })
+  .catch(err => {
+    if (err.message === "ENOENT: no such file or directory, open '"+TOKEN_FILE+"'")
+      console.warn('No stored token found, are you sure you generated one? Use option "-t" if you want to generate one next time.');
+    else
+      console.warn(err);
+
+    return err;
+  });
+}
+
 //imports a set of data. Data must be a map, with keys being a doctype, and the value an array of attributes maps.
 const importData = (cozyClient, data) => {
   for (const docType in data){
@@ -85,7 +104,7 @@ const importData = (cozyClient, data) => {
     
     Promise.all(docs.map(doc => (cozyClient.data.create(docType, doc))))
     .then(results => {
-      console.log('Imported ' + results.length + ' ' + docType + ' document(s)');
+      console.log('Imported ' + results.length + ' ' + docType + ' document' + (results.length > 1 ? 's' : ''));
       console.log(results.map(result => (result._id)));
     }, err => {
       if (err.name == 'FetchError' && err.status == 400){
@@ -101,39 +120,94 @@ const importData = (cozyClient, data) => {
   }
 }
 
+//drop all documents of the given doctype
+const dropCollections = (client, docTypes) => {
+  for (const docType of docTypes){
+    //we're going to need all the document ids and revisions, but for that we need an index
+    client.data.defineIndex(docType, ['_id'])
+    .then(index => {
+      //now let's fetch all the data
+      return client.data.query(index, {
+        selector: {'_id': {"$gt" : null}},
+        fields: ['_id', '_rev']
+      });
+    })
+    .then(docs => {
+      //well now we drop them all...
+      return Promise.all(docs.map(doc => (client.data.delete(docType, doc))))
+    })
+    .then(results => {
+      console.log('Deleted ' + results.filter(result => (result.deleted)).length + '/' + results.length + ' documents.');
+    })
+    .catch(err => {
+      console.warn(err);
+    });
+  }
+}
+
 //the CLI interface
 let program = require('commander');
 program
-  .version('0.1')
-  .option('-t --token', 'Generate a new token')
-  .option('-u --url', 'URL of the cozy to use. Defaults to "http://cozy.tools:8080".')
-  .option('-f --file', 'The file containing the JSON data to import. Defaults to "example-data.json".')
-  .parse(process.argv);
+.version('0.1')
+.option('-t --token', 'Generate a new token.')
+.option('-u --url', 'URL of the cozy to use. Defaults to "http://cozy.tools:8080".');
 
-//first preload the data to import. We'll need this to generate a token later
-const dataFile = program.file ? program.file.toString() : 'example-data.json';
-const data = JSON.parse(fs.readFileSync(dataFile));
+//impor command
+program.command('import [file]')
+.description('The file containing the JSON data to import. Defaults to "example-data.json".')
+.action(file => {
+  if (!file) file = 'example-data.json';
+  
+  //get the url of the cozy
+  const cozyUrl = program.url ? program.url.toString() : 'http://cozy.tools:8080';
+  
+  //collect the doctypes that we're going to import
+  let docTypes = [];
+  let data = JSON.parse(fs.readFileSync(file));
+  
+  for (let docType in data) {
+    docTypes.push(docType);
+  }
+  
+  //get a client
+  getClient(!!program.token, cozyUrl, docTypes)
+  .then(client => {
+    importData(client, data);
+  });
+});
 
-//build the list of doctypes we want to manipulate
-let docTypes = [];
-for (let docType in data) {
-  docTypes.push(docType);
+//is this a good idea?
+program.command('drop <doctypes...>')
+.description('Deletes all documents of the provided doctypes. For real.')
+.action(docTypes => {
+  const readline = require('readline');
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  rl.question('Are you sure? EVERY document of type ' + docTypes.join(', ') + ' will be removed. Type yes to confirm.\n', (answer) => {
+    rl.close();
+    
+    if (answer === 'yes'){
+      console.log('Okay, hang tight.');
+      //get the url of the cozy
+      const cozyUrl = program.url ? program.url.toString() : 'http://cozy.tools:8080';
+      
+      getClient(!!program.token, cozyUrl, docTypes)
+      .then(client => {
+        dropCollections(client, docTypes);
+      });
+    }
+    else {
+      console.log('Thought so.');
+    }
+  });
+})
+
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.help();
 }
-
-//define the target url
-const cozyUrl = program.url ? program.url.toString() : 'http://cozy.tools:8080';
-
-//now get a client
-let getClient = program.token ? getClientWithoutToken : getClientWithToken;
-
-getClient(cozyUrl, docTypes)
-.then(client => {
-  //client loaded! The only thing we support at the moment is importing the data file, so let's do that.
-  importData(client, data);
-})
-.catch(err => {
-  if (err.message === "ENOENT: no such file or directory, open '"+TOKEN_FILE+"'")
-    console.warn('No stored token found, are you sure you generated one? Use option "-t" if you want to generate one next time.');
-  else
-    console.warn(err);
-})
