75bf58e64f200dfa263dbafb6037340ee61c8fce
Patrick Browne <patrick@cozycloud.cc>
2018-02-16 13:06:01 +0100

    🐝 fix(api): supports database not existing

--- scripts/api.js
+++ scripts/api.js
@@ -2,11 +2,21 @@ const mkAPI = client => {
   
   const api = {
     fetchAll: async (doctype, queryOptions = '') => {
-      const result = await client.fetchJSON('GET', `/data/${doctype}/_all_docs?${queryOptions}`)
-      return result.rows.filter(x => x.id.indexOf('_design') !== 0)
+      try {
+        const result = await client.fetchJSON('GET', `/data/${doctype}/_all_docs?${queryOptions}`)
+        return result.rows.filter(x => x.id.indexOf('_design') !== 0)
+      } catch (e) {
+        if (e.reason.reason == 'Database does not exist.') {
+          return []
+        }
+        throw e
+      }
     },
 
     updateAll: async (doctype, docs) => {
+      if (!docs || docs.length === 0) {
+        return []
+      }
       return client.fetchJSON(
         'POST', `/data/${doctype}/_bulk_docs`,
         { docs }
