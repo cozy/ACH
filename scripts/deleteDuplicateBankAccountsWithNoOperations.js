e2f377a8078733735951ff4ea4e2496bcfe72675
Patrick Browne <pt.browne@gmail.com>
2018-11-09 18:44:36 +0100

    fix: Revert to using api : cozy-doctypes can't be used on several cozies

--- scripts/deleteDuplicateBankAccountsWithNoOperations.js
+++ scripts/deleteDuplicateBankAccountsWithNoOperations.js
@@ -1,24 +1,52 @@
-const { Document, BankAccount, BankTransaction } = require('cozy-doctypes')
+const mkAPI = require('./api')
+const groupBy = require('lodash/groupBy')
+const log = require('../libs/log')
 
-const run = async dryRun => {
-  const accounts = await BankAccount.fetchAll()
-  const operations = await BankTransaction.fetchAll()
-  const accountsWithNoOperations = BankAccount.findDuplicateAccountsWithNoOperations(
+const DOCTYPE_BANK_ACCOUNTS = 'io.cozy.bank.accounts'
+const DOCTYPE_BANK_TRANSACTIONS = 'io.cozy.bank.operations'
+
+const findDuplicateAccountsWithNoOperations = (accounts, operations) => {
+  const opsByAccountId = groupBy(operations, op => op.account)
+
+  const duplicateAccountGroups = Object.entries(
+    groupBy(accounts, x => x.institutionLabel + ' > ' + x.label)
+  )
+    .map(([, group]) => group)
+    .filter(group => group.length > 1)
+
+  const res = []
+  for (const duplicateAccounts of duplicateAccountGroups) {
+    for (const account of duplicateAccounts) {
+      const accountOperations = opsByAccountId[account._id] || []
+      log.info(
+        `Account ${account._id} has ${accountOperations.length} operations`
+      )
+      if (accountOperations.length === 0) {
+        res.push(account)
+      }
+    }
+  }
+  return res
+}
+
+const run = async (api, dryRun) => {
+  const accounts = await api.fetchAll(DOCTYPE_BANK_ACCOUNTS)
+  const operations = await api.fetchAll(DOCTYPE_BANK_TRANSACTIONS)
+  const accountsWithNoOperations = findDuplicateAccountsWithNoOperations(
     accounts,
     operations
   )
   const info = {
     deletedAccounts: accountsWithNoOperations.map(x => ({
       label: x.label,
-      _id: x._id,
-      shortLabel: x.shortLabel
+      _id: x._id
     }))
   }
   try {
     if (dryRun) {
       info.dryRun = true
     } else {
-      await BankAccount.deleteAll(accountsWithNoOperations)
+      api.deleteAll(DOCTYPE_BANK_ACCOUNTS, accountsWithNoOperations)
       info.success = true
     }
   } catch (e) {
@@ -32,11 +60,11 @@ const run = async dryRun => {
 
 module.exports = {
   getDoctypes: function() {
-    return [BankAccount.doctype, BankTransaction.doctype]
+    return [DOCTYPE_BANK_ACCOUNTS, DOCTYPE_BANK_TRANSACTIONS]
   },
+  findDuplicateAccountsWithNoOperations,
   run: async function(ach, dryRun = true) {
-    Document.registerClient(ach.client)
-    return run(dryRun).catch(err => {
+    return run(mkAPI(ach.client), dryRun).catch(err => {
       return {
         error: {
           message: err.message,
