1e82e398befa2402b6ed15ec36bea082dfa95375
Patrick Browne <pt.browne@gmail.com>
2019-03-04 11:37:18 +0100

    feat: Load relative or built-in scripts
    `batch` and `script` can load relative scripts. An error
    is thrown if no script or more than 1 script is found.

--- libs/scripts.js
+++ libs/scripts.js
@@ -3,14 +3,25 @@ const path = require('path')
 
 /** Requires builtin script or from relative path */
 const requireScript = scriptName => {
-  const dir = path.join(__dirname, 'scripts')
-  let script
-  try {
-    script = require(path.join(dir, scriptName))
-  } catch (e) {
-    console.log(e)
-    console.error(`${scriptName} does not exist in ${dir}`)
-    process.exit(1)
+  const results = [
+    path.resolve(scriptName),
+    path.join(__dirname, '../scripts', scriptName)
+  ].map(path => {
+    try {
+      return { ok: true, path, script: require(path) }
+    } catch (e) {
+      return { err: true, path: path }
+    }
+  })
+  const ok = results.filter(result => result.ok)
+  if (ok.length === 0) {
+    console.error(`Tried ${results.map(x => x.path).join(',')}`)
+    throw new Error(`No script found for name ${scriptName}`)
+  } else if (ok.length === 1) {
+    return ok[0].script
+  } else {
+    console.error(`Conflicting paths: ${ok.map(x => x.path).join(',')}`)
+    throw new Error(`Conflicting scripts found for ${scriptName}`)
   }
 }
 
