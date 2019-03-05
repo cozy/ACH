9feb670fd2a19fcd4cf072ead58431e310347231
Cédric Messiant <cedric.messiant@gmail.com>
2019-03-05 17:41:47 +0100

    feat: Add v1 to v2 migration for contacts

---
+++ scripts/contacts/migrateContactsV1toV2.js
@@ -0,0 +1,176 @@
+const has = require('lodash/has')
+const isEmpty = require('lodash/isEmpty')
+const omit = require('lodash/omit')
+
+const mkAPI = require('../api')
+const {
+  migrationDiff: diff,
+  getWithInstanceLogger
+} = require('../../libs/utils')
+
+const CONTACTS_APP_NAME = 'Contacts'
+const KONNECTOR_APP_NAME = 'konnector-google'
+const ACCOUNT_APP_VERSION = 1
+const DOCTYPE_CONTACTS = 'io.cozy.contacts'
+const DOCTYPE_ACCOUNTS = 'io.cozy.accounts'
+const DOCTYPE_CONTACTS_ACCOUNT = 'io.cozy.contacts.accounts'
+const DOCTYPE_CONTACTS_VERSION = 2
+
+const getCreatedByApp = contact => {
+  const metadataKeys = Object.keys(contact.metadata || {}).filter(
+    k => k !== 'version'
+  )
+
+  if (metadataKeys.includes('google')) {
+    return KONNECTOR_APP_NAME
+  } else {
+    return CONTACTS_APP_NAME
+  }
+}
+
+const filterContact = contact => {
+  const filteredContact = omit(
+    contact,
+    'vendorId',
+    'metadata.cozy',
+    'metadata.version'
+  )
+  if (isEmpty(filteredContact.metadata)) {
+    filteredContact.metadata = undefined
+  }
+
+  return filteredContact
+}
+
+const migrateContactV1toV2 = (contact, accountId, contactAccountId) => {
+  const now = new Date().toISOString()
+  const createdByApp = getCreatedByApp(contact)
+  let accounts = []
+  let sourceAccount, sync
+  if (contactAccountId && createdByApp === KONNECTOR_APP_NAME) {
+    sync = {
+      [contactAccountId]: {
+        contactsAccountsId: contactAccountId,
+        id: contact.vendorId,
+        konnector: KONNECTOR_APP_NAME,
+        lastSync: now,
+        remoteRev: undefined
+      }
+    }
+    sourceAccount = accountId
+    accounts = [
+      {
+        _id: contactAccountId,
+        _type: DOCTYPE_CONTACTS_ACCOUNT
+      }
+    ]
+  }
+
+  return {
+    ...filterContact(contact),
+    me: false,
+    cozyMetadata: {
+      sync,
+      createdAt: undefined,
+      createdByApp,
+      createdByAppVersion: undefined,
+      doctypeVersion: DOCTYPE_CONTACTS_VERSION,
+      sourceAccount,
+      updatedAt: now,
+      updatedByApps: [
+        {
+          date: now,
+          slug: createdByApp,
+          version: undefined
+        }
+      ]
+    },
+    relationships: {
+      ...contact.relationships,
+      accounts: {
+        data: accounts
+      }
+    }
+  }
+}
+
+const createContactAccount = (account, accountName) => ({
+  canLinkContacts: true,
+  shouldSyncOrphan: true,
+  lastSync: null,
+  lastLocalSync: null,
+  name: accountName,
+  type: KONNECTOR_APP_NAME,
+  sourceAccount: account._id,
+  version: ACCOUNT_APP_VERSION
+})
+
+const getAccountName = contacts => {
+  const contactWithFrom = contacts.find(contact =>
+    has(contact, 'metadata.google.from')
+  )
+  return contactWithFrom ? contactWithFrom.metadata.google.from : ''
+}
+
+async function doMigrations(dryRun, api, logWithInstance) {
+  const accounts = await api.fetchAll(DOCTYPE_ACCOUNTS)
+  const contacts = await api.fetchAll(DOCTYPE_CONTACTS)
+  const konnectorAccount = accounts.find(doc => doc.account_type === 'google')
+  const accountName = getAccountName(contacts)
+
+  let accountId, contactAccountId
+  if (dryRun) {
+    accountId = 'dryRunAccountId'
+    contactAccountId = 'dryRunContactAccountId'
+    const contactAccount = createContactAccount({ _id: accountId }, accountName)
+    logWithInstance('Dry run: would create contact account', contactAccount)
+  } else if (konnectorAccount) {
+    const contactAccount = createContactAccount(konnectorAccount, accountName)
+    const response = await api.updateAll(DOCTYPE_CONTACTS_ACCOUNT, [
+      contactAccount
+    ])
+    accountId = konnectorAccount._id
+    contactAccountId = response[0].id
+  }
+
+  const updatedContacts = contacts.map(contact =>
+    migrateContactV1toV2(contact, accountId, contactAccountId)
+  )
+
+  if (!dryRun) {
+    await api.updateAll(DOCTYPE_CONTACTS, updatedContacts)
+  } else {
+    logWithInstance(
+      'Dry run: first updated contact\n',
+      diff(contacts[0], updatedContacts[0])
+    )
+  }
+
+  logWithInstance(
+    dryRun ? 'Would create' : 'Has created',
+    1,
+    DOCTYPE_CONTACTS_ACCOUNT
+  )
+
+  logWithInstance(
+    dryRun ? 'Would update' : 'Has updated',
+    contacts.length,
+    DOCTYPE_CONTACTS
+  )
+}
+
+module.exports = {
+  getDoctypes: function() {
+    return [DOCTYPE_ACCOUNTS, DOCTYPE_CONTACTS, DOCTYPE_CONTACTS_ACCOUNT]
+  },
+
+  run: async function(ach, dryRun = true) {
+    const api = mkAPI(ach.client)
+    const logWithInstance = getWithInstanceLogger(ach.client)
+    try {
+      await doMigrations(dryRun, api, logWithInstance)
+    } catch (err) {
+      console.log(ach.url, err)
+    }
+  }
+}
---
+++ scripts/contacts/migrateContactsV1toV2.spec.js
@@ -0,0 +1,253 @@
+const MockDate = require('mockdate')
+
+const utils = require('../../libs/utils')
+const migrateContactsV1toV2 = require('./migrateContactsV1toV2')
+
+jest.mock('../../libs/utils')
+
+const fetchJSONSpy = jest.fn()
+
+const ACCOUNT_ID = '39c86201-8dda-489c-b313-eaa06ca4fe95'
+const CONTACT_ACCOUNT_ID = 'ee942dd5-a562-40b4-864d-162563985639'
+const MOCKED_DATE = '2017-03-04T08:28:24.054Z'
+
+const accountsFixture = {
+  rows: [
+    {
+      id: 'f0412ccf-aaf2-444c-922f-a7c5e8926705',
+      doc: {
+        account_type: 'whatever'
+      }
+    },
+    {
+      id: ACCOUNT_ID,
+      doc: {
+        _id: ACCOUNT_ID,
+        _rev: '2-8370061d-925b-40ee-b1d5-463d2049ae3f',
+        account_type: 'google',
+        auth: {},
+        name: '',
+        oauth: {
+          access_token: 'xxxxxxxx',
+          expires_at: '2019-03-04T15:28:24.054Z',
+          refresh_token: '1/xxxxxxxx',
+          token_type: 'Bearer'
+        },
+        oauth_callback_results: {
+          id_token: '',
+          scope:
+            'https://www.googleapis.com/auth/contacts.readonly openid https://www.googleapis.com/auth/userinfo.email'
+        }
+      }
+    }
+  ]
+}
+
+const contactsFixture = {
+  rows: [
+    // contact imported from google:
+    {
+      id: '0241b297-0df8-4137-a072-30d1736b0cc4',
+      doc: {
+        name: { familyName: 'Runolfsson', givenName: 'Carole' },
+        phone: [],
+        email: [],
+        address: [],
+        metadata: {
+          version: 1,
+          google: { metadata: {}, from: 'appvengers.cedric@gmail.com' }
+        },
+        vendorId: 'people/c104447654193047000'
+      }
+    },
+    // contact created in Contacts app:
+    {
+      id: '62726133-20a0-4d3b-bd2f-bf67836e81d9',
+      doc: {
+        name: { familyName: 'Emilia', givenName: 'Hilpert' },
+        phone: [],
+        email: [],
+        address: [],
+        metadata: {
+          cozy: true,
+          version: 1
+        }
+      }
+    },
+    // contact imported from vcard
+    {
+      id: '6ce9fc8c-d1ce-49e5-bfc7-be263c55e606',
+      doc: {
+        name: { familyName: 'Hector', givenName: 'Stark' },
+        phone: [],
+        email: [],
+        address: [],
+        metadata: {
+          version: 1
+        }
+      }
+    }
+  ]
+}
+
+beforeAll(() => {
+  MockDate.set(MOCKED_DATE)
+})
+
+afterAll(() => {
+  MockDate.reset()
+})
+
+describe('Contacts: migrate cozy metadata (konnector v1 to v2)', () => {
+  const fakeClient = {
+    fetchJSON: fetchJSONSpy
+  }
+  const fakeACH = {
+    client: fakeClient
+  }
+  const logWithInstanceSpy = jest.fn()
+
+  beforeEach(() => {
+    utils.getWithInstanceLogger.mockReturnValue(logWithInstanceSpy)
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('should migrate cozy metadata', async () => {
+    fetchJSONSpy.mockResolvedValueOnce(accountsFixture)
+    fetchJSONSpy.mockResolvedValueOnce(contactsFixture)
+
+    // create contact account
+    fetchJSONSpy.mockResolvedValueOnce([
+      {
+        id: CONTACT_ACCOUNT_ID
+      }
+    ])
+
+    await migrateContactsV1toV2.run(fakeACH, false)
+
+    expect(fetchJSONSpy).toHaveBeenCalledTimes(4)
+
+    const expectedDocs = [
+      {
+        cozyMetadata: {
+          doctypeVersion: 2,
+          createdAt: undefined,
+          createdByApp: 'konnector-google',
+          createdByAppVersion: undefined,
+          updatedByApps: [
+            {
+              date: MOCKED_DATE,
+              slug: 'konnector-google'
+            }
+          ],
+          updatedAt: MOCKED_DATE,
+          sourceAccount: ACCOUNT_ID,
+          sync: {
+            [CONTACT_ACCOUNT_ID]: {
+              konnector: 'konnector-google',
+              lastSync: MOCKED_DATE,
+              contactsAccountsId: CONTACT_ACCOUNT_ID,
+              id: 'people/c104447654193047000',
+              remoteRev: undefined
+            }
+          }
+        },
+        me: false,
+        name: { familyName: 'Runolfsson', givenName: 'Carole' },
+        phone: [],
+        email: [],
+        address: [],
+        metadata: {
+          google: { metadata: {}, from: 'appvengers.cedric@gmail.com' }
+        },
+        relationships: {
+          accounts: {
+            data: [
+              {
+                _id: CONTACT_ACCOUNT_ID,
+                _type: 'io.cozy.contacts.accounts'
+              }
+            ]
+          }
+        }
+      },
+      {
+        cozyMetadata: {
+          doctypeVersion: 2,
+          createdAt: undefined,
+          createdByApp: 'Contacts',
+          createdByAppVersion: undefined,
+          updatedByApps: [
+            {
+              date: MOCKED_DATE,
+              slug: 'Contacts',
+              version: undefined
+            }
+          ],
+          updatedAt: MOCKED_DATE,
+          sourceAccount: undefined,
+          sync: undefined
+        },
+        me: false,
+        name: { familyName: 'Emilia', givenName: 'Hilpert' },
+        phone: [],
+        email: [],
+        address: [],
+        relationships: {
+          accounts: {
+            data: []
+          }
+        }
+      },
+      {
+        cozyMetadata: {
+          doctypeVersion: 2,
+          createdAt: undefined,
+          createdByApp: 'Contacts',
+          createdByAppVersion: undefined,
+          updatedByApps: [
+            {
+              date: MOCKED_DATE,
+              slug: 'Contacts',
+              version: undefined
+            }
+          ],
+          updatedAt: MOCKED_DATE,
+          sourceAccount: undefined,
+          sync: undefined
+        },
+        me: false,
+        name: { familyName: 'Hector', givenName: 'Stark' },
+        phone: [],
+        email: [],
+        address: [],
+        relationships: {
+          accounts: {
+            data: []
+          }
+        }
+      }
+    ]
+    expect(fetchJSONSpy).toHaveBeenNthCalledWith(
+      4,
+      'POST',
+      '/data/io.cozy.contacts/_bulk_docs',
+      { docs: expectedDocs }
+    )
+  })
+
+  it('should not update data in dry run mode', async () => {
+    fetchJSONSpy.mockResolvedValueOnce(accountsFixture)
+    fetchJSONSpy.mockResolvedValueOnce(contactsFixture)
+
+    await migrateContactsV1toV2.run(fakeACH, true)
+
+    expect(fetchJSONSpy).toHaveBeenCalledTimes(2)
+    expect(fetchJSONSpy.mock.calls[0][0]).toEqual('GET')
+    expect(fetchJSONSpy.mock.calls[1][0]).toEqual('GET')
+    expect(logWithInstanceSpy).toHaveBeenCalledTimes(4)
+  })
+})
