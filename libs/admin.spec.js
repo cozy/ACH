3dafae78cfef63bcad9251b13ee340ad47362282
Patrick Browne <pt.browne@gmail.com>
2019-10-09 16:25:20 +0200

    feat: withEnvTunnel supports multiple tunnels

--- libs/admin.spec.js
+++ libs/admin.spec.js
@@ -1,8 +1,41 @@
 jest.mock('node-fetch')
-jest.mock('./config')
+jest.mock('./config', () => ({
+  loadConfig: jest.fn(),
+  getAdminConfigForEnv: env => {
+    console.lo
+    if (env === 'prod') {
+      return {
+        adminURL: 'http://localhost:6065',
+        host: 'gozy-adm-prod'
+      }
+    } else if (env === 'dev') {
+      return {
+        adminURL: 'http://localhost:6061',
+        host: 'gozy-adm-dev'
+      }
+    } else {
+      throw new Error(`Unknown env ${JSON.stringify(env)} in tests`)
+    }
+  }
+}))
 
+jest.mock('child_process', () => {
+  const fakeEventEmitter = () => ({
+    on: jest.fn(),
+    off: jest.fn()
+  })
+  return {
+    spawn: jest.fn().mockReturnValue({
+      stdout: fakeEventEmitter(),
+      stderr: fakeEventEmitter(),
+      kill: jest.fn()
+    })
+  }
+})
+
+const { spawn } = require('child_process')
 const fetch = require('node-fetch')
-const { createToken } = require('./admin')
+const { createToken, withEnvTunnel } = require('./admin')
 
 describe('admin', () => {
   beforeEach(() => {
@@ -28,5 +61,38 @@ describe('admin', () => {
         }
       }
     )
+  })
+})
+
+describe('withEnvTunnel', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'log').mockImplementation(() => {})
+  })
+  it('should call correctly ssh', async () => {
+    const cb = jest.fn().mockResolvedValue('ret-value')
+    const res = await withEnvTunnel(['prod', 'dev'], cb)
+    expect(res).toBe('ret-value')
+    expect(spawn).toHaveBeenCalledWith('ssh', [
+      '-tt',
+      '-fN',
+      '-L',
+      '6065:gozy-adm-prod:6060',
+      '-L',
+      '6061:gozy-adm-dev:6060',
+      'bounce2'
+    ])
+  })
+
+  it('shorthand version should work', async () => {
+    const cb = jest.fn().mockResolvedValue('ret-value')
+    const res = await withEnvTunnel(['prod'], cb)
+    expect(res).toBe('ret-value')
+    expect(spawn).toHaveBeenCalledWith('ssh', [
+      '-tt',
+      '-fN',
+      '-L',
+      '6065:gozy-adm-prod:6060',
+      'bounce2'
+    ])
   })
 })
